4c2c8620e8e1811e42dd6fd6ef1d92e32414952d
MyMel2001 Holder <163201376+mymel2001-holder@users.noreply.github.com>
2026-04-10 23:51:00 -0500

    Delete log/new-post.sh

--- log/new-post.sh
+++
@@ -1,9 +0,0 @@
-postID="$RANDOM-$RANDOM$RANDOM"
-
-echo " " > $postID
-echo "## $(date) - Title" >> $postID
-echo "Body" >> $postID
-nano $postID
-echo " " >> $postID
-cat $postID >> index.gmi
-rm $postID
